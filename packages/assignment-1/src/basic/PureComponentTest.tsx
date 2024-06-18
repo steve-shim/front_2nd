07cdaed76041fc711389cde0674b2a4697f133a3
steve-shim <steveshim496@gmail.com>
2024-06-18 19:07:26 +0900

    Commit PureComponent

--- packages/assignment-1/src/basic/PureComponentTest.tsx
+++ packages/assignment-1/src/basic/PureComponentTest.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, memo } from "react";
 import { Cat, Dog } from "./PureComponentTest.components.tsx";
 
-
+const PureCatComponent = memo(Cat);
+const PureDogComponent = memo(Dog);
 // NOTE: 다른 파일은 수정하지 않고, 현재 파일만 수정하여 문제를 해결해주세요.
 export default function PureComponentTest() {
   const [meowCount, setMeowCount] = useState(1);
@@ -9,10 +10,14 @@ export default function PureComponentTest() {
 
   return (
     <div>
-      <Cat crying={meowCount}/>
-      <Dog crying={barkedCount}/>
-      <button data-testid="meow" onClick={() => setMeowCount(n => n + 1)}>야옹</button>
-      <button data-testid="bark" onClick={() => setBarkedCount(n => n + 1)}>멍멍</button>
+      <PureCatComponent crying={meowCount} />
+      <PureDogComponent crying={barkedCount} />
+      <button data-testid="meow" onClick={() => setMeowCount((n) => n + 1)}>
+        야옹
+      </button>
+      <button data-testid="bark" onClick={() => setBarkedCount((n) => n + 1)}>
+        멍멍
+      </button>
     </div>
   );
 }
