1c46daafee12cc7d8487b20dfcb1f0c1fa380fec
steve-shim <steveshim496@gmail.com>
2024-06-18 19:08:16 +0900

    Commit useMyRef

--- packages/assignment-1/src/advanced/useMyRef.ts
+++ packages/assignment-1/src/advanced/useMyRef.ts
@@ -1,3 +1,11 @@
-export function useMyRef<T>(initValue: T | null) {
-  return { current: initValue }
+import { useMemo } from "react";
+
+interface RefObject<T> {
+  current: T | null;
+}
+
+export function useMyRef<T>(initValue: T | null): RefObject<T> {
+  const item = { current: initValue };
+  const ref = useMemo(() => item, []);
+  return ref;
 }
