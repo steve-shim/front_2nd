576e205cd8bc0fe4900a08a0e6cc46cfe28a451e
steve-shim <steveshim496@gmail.com>
2024-06-18 19:07:00 +0900

    Commit useCallback

--- packages/assignment-1/src/basic/UseCallbackTest.tsx
+++ packages/assignment-1/src/basic/UseCallbackTest.tsx
@@ -1,16 +1,26 @@
-import { useState } from "react";
+import React, { useState, memo, useCallback } from "react";
 import { BarkButton, MeowButton } from "./UseCallbackTest.components.tsx";
 
+const PureCatComponent = memo(MeowButton);
+const PureDogComponent = memo(BarkButton);
 export default function UseCallbackTest() {
   const [meowCount, setMeowCount] = useState(0);
   const [barkedCount, setBarkedCount] = useState(0);
+
+  const generateMeowCount = useCallback(() => {
+    setMeowCount((n) => n + 1);
+  }, []);
+
+  const generateBarkedCount = useCallback(() => {
+    setBarkedCount((n) => n + 1);
+  }, []);
 
   return (
     <div>
       <p data-testid="cat">meowCount {meowCount}</p>
       <p data-testid="dog">barkedCount {barkedCount}</p>
-      <MeowButton onClick={() => setMeowCount(n => n + 1)}/>
-      <BarkButton onClick={() => setBarkedCount(n => n + 1)}/>
+      <PureCatComponent onClick={generateMeowCount} />
+      <PureDogComponent onClick={generateBarkedCount} />
     </div>
   );
 }
