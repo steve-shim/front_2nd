c6d1b164817edc3d5d1cda35017f1145de0e04f5
SteveShim <steveshim496@gmail.com>
2024-06-28 08:04:00 +0900

    feat: 2주차 심화과제 구현

--- packages/assignment-2/src/advanced/advanced.jsx
+++ packages/assignment-2/src/advanced/advanced.jsx
@@ -1,13 +1,38 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useRef } from "react";
+import { deepEquals } from "../basic/basic";
 
-export const memo1 = (fn) => fn();
+const cache = new Map();
+export const memo1 = (fn) => {
+  if (cache.has(fn)) {
+    return cache.get(fn);
+  }
+  const res = fn();
+  cache.set(fn, res);
+  return res;
+};
 
-export const memo2 = (fn) => fn();
-
+export const memo2 = (fn, arr) => {
+  const key = arr.toString();
+  if (cache.has(key)) {
+    return cache.get(key);
+  }
+  const res = fn();
+  cache.set(key, res);
+  return res;
+};
 
 export const useCustomState = (initValue) => {
-  return useState(initValue);
-}
+  const [currentState, setCurrentState] = useState(initValue);
+
+  const compareFunction = (newValue) => {
+    if (deepEquals(currentState, newValue)) {
+      return;
+    } else {
+      setCurrentState(newValue);
+    }
+  };
+  return [currentState, compareFunction];
+};
 
 const textContextDefaultValue = {
   user: null,
@@ -21,42 +46,45 @@ export const TestContext = createContext({
 });
 
 export const TestContextProvider = ({ children }) => {
-  const [value, setValue] = useState(textContextDefaultValue);
+  const valueRef = useRef(textContextDefaultValue);
+
+  const isChangeRealValue = (beforeState, nowState, key) => {
+    let _result = !deepEquals(beforeState, nowState);
+    if (_result) {
+      valueRef.current[key] = nowState;
+    }
+    return _result || nowState === textContextDefaultValue[key];
+  };
 
   return (
-    <TestContext.Provider value={{ value, setValue }}>
+    <TestContext.Provider
+      value={{ value: valueRef.current, isChangeRealValue }}
+    >
       {children}
     </TestContext.Provider>
-  )
-}
+  );
+};
 
-const useTestContext = () => {
-  return useContext(TestContext);
-}
+const useTestContext = (key) => {
+  const { value, isChangeRealValue } = useContext(TestContext);
+
+  const [state, setState] = useState(value[key]);
+
+  if (isChangeRealValue(value[key], state, key)) {
+    return [state, setState];
+  } else {
+    return [state, () => {}];
+  }
+};
 
 export const useUser = () => {
-  const { value, setValue } = useTestContext();
-
-  return [
-    value.user,
-    (user) => setValue({ ...value, user })
-  ];
-}
+  return useTestContext("user");
+};
 
 export const useCounter = () => {
-  const { value, setValue } = useTestContext();
-
-  return [
-    value.count,
-    (count) => setValue({ ...value, count })
-  ];
-}
+  return useTestContext("count");
+};
 
 export const useTodoItems = () => {
-  const { value, setValue } = useTestContext();
-
-  return [
-    value.todoItems,
-    (todoItems) => setValue({ ...value, todoItems })
-  ];
-}
+  return useTestContext("todoItems");
+};
