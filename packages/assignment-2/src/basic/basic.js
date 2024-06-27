975415715b20cfd72edad9f155bfb6235f925b28
SteveShim <steveshim496@gmail.com>
2024-06-28 00:14:10 +0900

    feat: deepEquals 구현

--- packages/assignment-2/src/basic/basic.js
+++ packages/assignment-2/src/basic/basic.js
@@ -48,7 +48,32 @@ export function shallowEquals(target1, target2) {
 }
 
 export function deepEquals(target1, target2) {
-  return target1 === target2;
+  let targetList = [];
+  function checkObjValue(t1, t2) {
+    if (
+      (t1?.constructor !== t2?.constructor &&
+        Object.prototype.toString.call(t1) !== "[object Array]") ||
+      Object.prototype.toString.call(t1) === "[object Undefined]" ||
+      Object.prototype.toString.call(t1) === "[object Null]" ||
+      Object.prototype.toString.call(t1) === "[object Number]" ||
+      Object.prototype.toString.call(t1) === "[object String]"
+    )
+      targetList.push(shallowEquals(t1, t2));
+    if (t1?.length !== t2?.length) {
+      targetList.push(false);
+    }
+    if (typeof t1 === "object") {
+      for (const key in t1) {
+        if (Object.keys(t1[key]).length) {
+          checkObjValue(t1[key], t2[key]);
+        } else {
+          targetList.push(shallowEquals(t1[key], t2[key]));
+        }
+      }
+    }
+  }
+  checkObjValue(target1, target2);
+  return targetList.every((v) => v);
 }
 
 export function createNumber1(n) {
