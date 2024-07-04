1081c3f37d2c575037d8e3877f2f48aa3f0adefd
SteveShim <steveshim496@gmail.com>
2024-07-05 07:15:12 +0900

    feat : MyReact js 구현

--- packages/assignment-3/src/basic/MyReact.js
+++ packages/assignment-3/src/basic/MyReact.js
@@ -2,10 +2,30 @@ import { createHooks } from "./hooks";
 import { render as updateElement } from "./render";
 
 function MyReact() {
-  const _render = () => {};
-  function render($root, rootComponent) {}
+  let component = null;
+  let oldNode = null;
+  let root = null;
 
-  const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
+  const _render = () => {
+    resetHookContext();
+    const newNode = component();
+    updateElement(root, newNode, oldNode);
+    oldNode = newNode;
+  };
+
+  function render($root, rootComponent) {
+    oldNode = null;
+    root = $root;
+    component = rootComponent;
+
+    _render();
+  }
+
+  const {
+    useState,
+    useMemo,
+    resetContext: resetHookContext,
+  } = createHooks(_render);
 
   return { render, useState, useMemo };
 }
