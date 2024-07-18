8acc44dad9df6ef3d5834347894bbd5598ed4a48
steve-shim <steveshim496@gmail.com>
2024-07-19 00:28:59 +0900

    feat: useCoupon 구현

--- packages/assignment-5/src/refactoring/hooks/useCoupon.ts
+++ packages/assignment-5/src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,20 @@
-import { Coupon } from "../../types.ts";
-import { useState } from "react";
+import { Coupon } from '../../types.ts';
+import { useState, useEffect } from 'react';
 
-export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+export const useCoupons = (
+  initialCoupons: Coupon[],
+  onAddCoupon?: (newCoupons: Coupon[]) => void
+) => {
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  // 쿠폰을 추가하는 함수
+  const addCoupon = (newCoupon: Coupon) => {
+    setCoupons(prevCoupons => [...prevCoupons, newCoupon]);
+  };
+
+  useEffect(() => {
+    onAddCoupon?.(coupons);
+  }, [coupons]);
+
+  return { coupons, addCoupon };
 };
