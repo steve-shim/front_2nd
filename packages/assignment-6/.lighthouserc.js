8707a23428a6b9acda56999a6df1b6f8dc2ba8fc
steve-shim <steveshim496@gmail.com>
2024-07-26 16:00:28 +0900

    rc

---
+++ packages/assignment-6/.lighthouserc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: "npm run start",
+      startServerReadyPattern: "ready on",
+      url: ["http://localhost:3000"],
+      numberOfRuns: 3,
+      settings: {
+        preset: "desktop",
+      },
+    },
+    upload: {
+      target: "filesystem",
+      outputDir: "./lhci_reports",
+      reportFilenamePattern: "%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%",
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:performance": ["error", { minScore: 0.8 }],
+        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:best-practices": ["error", { minScore: 0.9 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
+      },
+    },
+  },
+};
