75a3b8ab3d5027b93b3a9d15313c7b9f722e3089
steve-shim <steveshim496@gmail.com>
2024-08-22 11:06:43 +0900

    드래그시 렌더링 최적화

--- packages/assignment-10/src/basic/ScheduleTables.tsx
+++ packages/assignment-10/src/basic/ScheduleTables.tsx
@@ -1,8 +1,10 @@
-import { Button, ButtonGroup, Flex, Heading, Stack } from "@chakra-ui/react";
-import ScheduleTable from "./ScheduleTable.tsx";
-import { useScheduleContext } from "./ScheduleContext.tsx";
-import SearchDialog from "./SearchDialog.tsx";
-import { useState } from "react";
+import { Button, ButtonGroup, Flex, Heading, Stack } from '@chakra-ui/react';
+import ScheduleTable from './ScheduleTable.tsx';
+import { useScheduleContext } from './ScheduleContext.tsx';
+import SearchDialog from './SearchDialog.tsx';
+import { useState, memo } from 'react';
+import { deepEquals } from './utils.ts';
+import ScheduleDndProvider from './ScheduleDndProvider.tsx';
 
 export const ScheduleTables = () => {
   const { schedulesMap, setSchedulesMap } = useScheduleContext();
@@ -11,51 +13,77 @@ export const ScheduleTables = () => {
     day?: string;
     time?: number;
   } | null>(null);
-
-  const disabledRemoveButton = Object.keys(schedulesMap).length === 1
+  const disabledRemoveButton = Object.keys(schedulesMap).length === 1;
 
   const duplicate = (targetId: string) => {
     setSchedulesMap(prev => ({
       ...prev,
-      [`schedule-${Date.now()}`]: [...prev[targetId]]
-    }))
-  }
+      [`schedule-${Date.now()}`]: [...prev[targetId]],
+    }));
+  };
 
   const remove = (targetId: string) => {
     setSchedulesMap(prev => {
       delete prev[targetId];
       return { ...prev };
-    })
-  }
+    });
+  };
+
+  const MemoScheduleTable = ({ tableId, schedules, index }: Props) => {
+    console.log('MemotableId, schedules, index', tableId, schedules, index);
+    return (
+      <Stack key={tableId} width="600px">
+        <Flex justifyContent="space-between" alignItems="center">
+          <Heading as="h3" fontSize="lg">
+            시간표 {index + 1}
+          </Heading>
+          <ButtonGroup size="sm" isAttached>
+            <Button colorScheme="green" onClick={() => setSearchInfo({ tableId })}>
+              시간표 추가
+            </Button>
+            <Button colorScheme="green" mx="1px" onClick={() => duplicate(tableId)}>
+              복제
+            </Button>
+            <Button colorScheme="green" isDisabled={disabledRemoveButton} onClick={() => remove(tableId)}>
+              삭제
+            </Button>
+          </ButtonGroup>
+        </Flex>
+        <ScheduleTable
+          key={`schedule-table-${index}`}
+          schedules={schedules}
+          tableId={tableId}
+          onScheduleTimeClick={timeInfo => setSearchInfo({ tableId, ...timeInfo })}
+          onDeleteButtonClick={({ day, time }) =>
+            setSchedulesMap(prev => ({
+              ...prev,
+              [tableId]: prev[tableId].filter(schedule => schedule.day !== day || !schedule.range.includes(time)),
+            }))
+          }
+        />
+      </Stack>
+    );
+  };
+
+  const MemoScheduleTableTest = memo(MemoScheduleTable, (prevProps, nextProps) => {
+    // 깊은 비교 로직 구현
+    return (
+      deepEquals(prevProps.tableId, nextProps.tableId) &&
+      deepEquals(prevProps.schedules, nextProps.schedules) &&
+      deepEquals(prevProps.index, nextProps.index)
+    );
+  });
 
   return (
     <>
       <Flex w="full" gap={6} p={6} flexWrap="wrap">
         {Object.entries(schedulesMap).map(([tableId, schedules], index) => (
-          <Stack key={tableId} width="600px">
-            <Flex justifyContent="space-between" alignItems="center">
-              <Heading as="h3" fontSize="lg">시간표 {index + 1}</Heading>
-              <ButtonGroup size="sm" isAttached>
-                <Button colorScheme="green" onClick={() => setSearchInfo({ tableId })}>시간표 추가</Button>
-                <Button colorScheme="green" mx="1px" onClick={() => duplicate(tableId)}>복제</Button>
-                <Button colorScheme="green" isDisabled={disabledRemoveButton}
-                        onClick={() => remove(tableId)}>삭제</Button>
-              </ButtonGroup>
-            </Flex>
-            <ScheduleTable
-              key={`schedule-table-${index}`}
-              schedules={schedules}
-              tableId={tableId}
-              onScheduleTimeClick={(timeInfo) => setSearchInfo({ tableId, ...timeInfo })}
-              onDeleteButtonClick={({ day, time }) => setSchedulesMap((prev) => ({
-                ...prev,
-                [tableId]: prev[tableId].filter(schedule => schedule.day !== day || !schedule.range.includes(time))
-              }))}
-            />
-          </Stack>
+          <ScheduleDndProvider>
+            <MemoScheduleTableTest key={tableId} tableId={tableId} schedules={schedules} index={index} />
+          </ScheduleDndProvider>
         ))}
       </Flex>
-      <SearchDialog searchInfo={searchInfo} onClose={() => setSearchInfo(null)}/>
+      <SearchDialog searchInfo={searchInfo} onClose={() => setSearchInfo(null)} />
     </>
   );
-}
+};
--- packages/assignment-10/src/basic/utils.ts
+++ packages/assignment-10/src/basic/utils.ts
@@ -6,25 +6,43 @@ export const parseHnM = (current: number) => {
 };
 
 const getTimeRange = (value: string): number[] => {
-  const [start, end] = value.split("~").map(Number);
+  const [start, end] = value.split('~').map(Number);
   if (end === undefined) return [start];
   return Array(end - start + 1)
     .fill(start)
     .map((v, k) => v + k);
-}
+};
 
 export const parseSchedule = (schedule: string) => {
   const schedules = schedule.split('<p>');
   return schedules.map(schedule => {
-
     const reg = /^([가-힣])(\d+(~\d+)?)(.*)/;
 
     const [day] = schedule.split(/(\d+)/);
 
-    const range = getTimeRange(schedule.replace(reg, "$2"));
+    const range = getTimeRange(schedule.replace(reg, '$2'));
 
-    const room = schedule.replace(reg, "$4")?.replace(/\(|\)/g, "");
+    const room = schedule.replace(reg, '$4')?.replace(/\(|\)/g, '');
 
     return { day, range, room };
   });
 };
+
+export function deepEquals(target1, target2) {
+  console.log('target1, target2', target1, target2);
+  // 재귀적으로 내부 객체까지 비교
+  if (target1 === target2) {
+    return true;
+  }
+  if (Array.isArray(target1) && Array.isArray(target2) && target1.length === target2.length) {
+    return target1.every((v, k) => deepEquals(v, target2[k]));
+  }
+  if (target1.constructor === Object && target2.constructor === Object) {
+    const key1 = Object.keys(target1);
+    const key2 = Object.keys(target2);
+    return (
+      target1 === target2 || (key1.length === key2.length && key1.every(key => deepEquals(target1[key], target2[key])))
+    );
+  }
+  return false;
+}
