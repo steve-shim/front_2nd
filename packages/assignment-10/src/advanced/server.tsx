075f36f972817141499d812de9f8c62741e05227
steve-shim <steveshim496@gmail.com>
2024-08-22 21:10:35 +0900

    advanced 과제 구현

--- packages/assignment-10/src/advanced/server.tsx
+++ packages/assignment-10/src/advanced/server.tsx
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import React from 'react';
 import express from 'express';
 import ReactDOMServer from 'react-dom/server';
@@ -8,10 +6,22 @@ import { App } from './App.tsx';
 const app = express();
 const port = 3333;
 
-app.get('*', (req, res) => {
-  const app = ReactDOMServer.renderToString(<App url={req.url}/>);
+// 렌더링된 결과를 저장할 캐시 객체
+const renderCache = new Map();
 
-  res.send(`
+app.get('*', (req, res) => {
+  const url = req.url;
+
+  // 캐시에 해당 URL의 렌더링 결과가 있는지 확인
+  if (renderCache.has(url)) {
+    console.log(`Serving from cache for ${url}`);
+    return res.send(renderCache.get(url));
+  }
+
+  // 캐시에 없으면 새로 렌더링
+  const app = ReactDOMServer.renderToString(<App url={url} />);
+
+  const html = `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -23,7 +33,12 @@ app.get('*', (req, res) => {
       <div id="root">${app}</div>
     </body>
     </html>
-  `);
+  `;
+
+  // 렌더링 결과를 캐시에 저장
+  renderCache.set(url, html);
+
+  res.send(html);
 });
 
 app.listen(port, () => {
